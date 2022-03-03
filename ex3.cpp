c8c62c0183f3082cd38dd326a711a95d336fdad9
Chrisyahui4 <100842198+Chrisyahui4@users.noreply.github.com>
2022-03-03 19:17:04 +0800

    Create ex3.cpp

---
+++ ex3.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <stdio.h>
+using namespace std;
+int main()
+
+{   
+    int myage=16;
+    int myweight=65;
+    int myheight=185;
+    printf("I am %d years old.  \n  ", myage);
+    printf("My weight is %d kg. \n  ",myweight);
+    printf("I height is %d cm" ,myheight);
+    printf("I have %d brother. %d brother is University. %d is junior high school.\n",2,1,1);
+    printf("My parents are also learn programming. \n");
+    return 0;
+}
